194fd5c44565f7c4fe1f66a6a8849dd6cfb80add
Axel Cabrera <a01738945@tec.mx>
2025-03-25 21:59:00 -0600

    Got code working with test: Scores a guess with two Bulls

--- test/bulls_and_cows_test.exs
+++ test/bulls_and_cows_test.exs
@@ -9,4 +9,8 @@ defmodule BullsAndCowsTest do
     assert BullsAndCows.score_guess("1234", "1678") == "1 Bulls, 0 Cows"
     assert BullsAndCows.score_guess("2345", "6785") == "1 Bulls, 0 Cows"
   end
+  test "Scores a guess with two Bulls" do
+    assert BullsAndCows.score_guess("1234", "5236") == "2 Bulls, 0 Cows"
+    assert BullsAndCows.score_guess("2345", "2685") == "2 Bulls, 0 Cows"
+  end
 end

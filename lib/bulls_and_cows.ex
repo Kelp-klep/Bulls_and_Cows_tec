deaefd0e1168a6519236b355d9cb271fa0854986
Juan Pablo Hernandez <jupahear@hotmail.com>
2025-03-25 22:29:39 -0600

    Got code working with test: Scores a guess with one Cow

--- lib/bulls_and_cows.ex
+++ lib/bulls_and_cows.ex
@@ -2,6 +2,12 @@ defmodule BullsAndCows do
   def score_guess(secret, guess) do
     bulls = Enum.zip(String.graphemes(secret), String.graphemes(guess))
             |> Enum.count(fn {s, g} -> s == g end)
-    "#{bulls} Bulls, 0 Cows"
+
+    secret_remains = Enum.zip(String.graphemes(secret), String.graphemes(guess))
+            |> Enum.filter(fn {s,g} -> s != g end)
+            |> Enum.map (fn {s, _g} -> s end)
+
+    cows = Enum.count(String.graphemes(guess), fn g -> g in secret_remains end)
+    "#{bulls} Bulls, #{cows} Cows"
   end
 end
